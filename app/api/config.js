646c17fea991bbb85ea1b800d5465cd4ba21ac0b
toopooriik <is511723@gmail.com>
2024-05-23 23:39:30 +1000

    вв

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://toporik.nomoredomainswork.ru/api'
+export const BASE_URL = 'https://toporik.nomoredomainswork.ru'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
